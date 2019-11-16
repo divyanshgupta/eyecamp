7b20f80f512354c332c4bc8f116652cedfd01bf9
divyansh gupta <divyansh.gupta@iongroup.com>
2019-11-16 09:38:06 +0530

    passport auth flow implemented

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 /**Commonjs module */
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const keys = require('./config/keys')
 
 /*Es 2015 modules* */
 // import express from 'express'
 const app = express();
-app.get('/', (req, res) =>
-{
-    res.send({ hi: 'there again!!!' })
-})
+
+
+passport.use(new GoogleStrategy({
+    clientID:keys.googleClientID,
+    clientSecret:keys.googleClientSecret,
+    callbackURL:'/auth/google/callback'
+},(accessToken,refreshToken,profile,done)=>{
+    console.log('access',accessToken)
+    console.log('refresh',refreshToken)
+    console.log('profile',profile)
+    
+    
+}))
+
+// app.get('/', (req, res) =>
+// {
+//     res.send({ hi: 'there again!!!' })
+// })
+
+app.get('/auth/google',passport.authenticate('google',{
+    scope:['profile','email']
+}))
+
+app.get('/auth/google/callback',passport.authenticate('google'))
+
+
 
 const PORT = process.env.PORT || 5000;
 
