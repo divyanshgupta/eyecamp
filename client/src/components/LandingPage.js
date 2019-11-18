c3c5674057661db9d8e2168fb0e243530d17cfa5
divyansh gupta <divyansh.gupta@iongroup.com>
2019-11-18 22:55:52 +0530

    Adding Landing Page Component

---
+++ client/src/components/LandingPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Landing = ()=>
+{
+    return (
+        <div style={{textAlign:'center'}}>
+            <h1>GyanSangam 2019!!</h1>
+            Create your new survey!!
+        </div>
+    )
+}
+
+export default Landing;
